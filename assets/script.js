d77dd93fecfffa7fcda515e1ee4f34926282daf9
Zig <amalric.alex@gmail.com>
2023-12-18 17:09:30 +0100

    nouvelle version fonctionelle

--- assets/script.js
+++ assets/script.js
@@ -20,75 +20,96 @@ const slides = [
 //initialisation du parent
 const banner = document.getElementById("banner")
 
-// initialisation de la magie
-let i = 0
-
 //initialisation des dots
-let dots = document.querySelector(".dots")
-for (let index = 0; index < slides.length; index++) {
+let dots = banner.querySelector(".dots")
+
+//création des dot
+slides.forEach(() => {
 	let dot = document.createElement("div")
 	dot.classList.add("dot")
 	dots.append(dot)
-}
+});
 
 //création de l'array de dots length===slides.length
 const dotList = Array.from(dots.children)
+
+//initialisation de l'index
+let indexPosition = 0
+
 //initialization de dot_selected
 dotList[0].classList.add("dot_selected")
 
-//création des flèches
-let arrowLeft = document.createElement("img")
-arrowLeft.classList.add("arrow", "arrow_left")
-arrowLeft.src = "./assets/images/arrow_left.png"
-banner.appendChild(arrowLeft)
+//simulation du modulo via l'opérateur remainder
+function modulo(value) {
+	return ((value % slides.length) + slides.length) % slides.length
+}
 
-let arrowRight = document.createElement("img")
-arrowRight.classList.add("arrow", "arrow_right")
-arrowRight.src = "./assets/images/arrow_right.png"
-banner.appendChild(arrowRight)
+//initialization des flèches v2
+// let arrowsNodeList = banner.querySelectorAll(".arrow")
+// let arrows = Array.from(banner.querySelectorAll(".arrow"))
+
+//initialisation des flèches
+let arrowLeft = banner.querySelector(".arrow_left")
+let arrowRight = banner.querySelector(".arrow_right")
 
 //fonction changeSlide
 function changeSlide(slideObjectImg, slideObjectTagLine) {
-	let bannerImg = document.querySelector(".banner-img")
+	let bannerImg = banner.querySelector(".banner-img")
 	bannerImg.src = "./assets/images/slideshow/" + slideObjectImg;
 	let bannerTagLine = banner.getElementsByTagName("p")
 	bannerTagLine[0].innerHTML = slideObjectTagLine
 }
 
+//mise a jour the contenu de la nouvelle slide
+function updateSlideContent() {
+	changeSlide(slides[modulo(indexPosition)].image, slides[modulo(indexPosition)].tagLine)
+}
+
+//retrait de la class .dot_selected avant le changement de slide
+function removeCurrentSelectedDot() {
+	dotList[modulo(indexPosition)].classList.remove("dot_selected")
+}
+
+//ajout de la class .dot_selected a la slide actuelle
+function addCurrentSelectedDot() {
+	dotList[modulo(indexPosition)].classList.add("dot_selected")
+}
+
+//décrémentation de l'index pour passer a la slide précédente
+function moveToPreviousSlide() {
+	indexPosition--
+}
+
+//incrémentation de l'index pour passer a la slide suivante
+function moveToNextSlide() {
+	indexPosition++
+}
+
 //eventListener des flèches
-//gauche
+
 arrowLeft.addEventListener("click", () => {
 	console.log("le bouton gauche est cliqué")
-	i--
-	if (i < 0) {
-		i = slides.length - 1
-		dotList[0].classList.remove("dot_selected") //retire la précédente
-		dotList[i].classList.add("dot_selected") //ajoute la suivante
-		changeSlide(slides[i].image, slides[i].tagLine)
-		console.log(i)
-	}
-	else {
-		dotList[i + 1].classList.remove("dot_selected")
-		dotList[i].classList.add("dot_selected")
-		changeSlide(slides[i].image, slides[i].tagLine)
-		console.log(i)
-	}
+	removeCurrentSelectedDot()
+	moveToPreviousSlide()
+	addCurrentSelectedDot()
+	updateSlideContent()
 });
-//droite
+
 arrowRight.addEventListener("click", () => {
-	console.log("le bouton droit est cliqué")
-	i++
-	if (i > (slides.length - 1)) {
-		i = 0
-		dotList[slides.length - 1].classList.remove("dot_selected")
-		dotList[i].classList.add("dot_selected")
-		changeSlide(slides[i].image, slides[i].tagLine)
-		console.log(i)
-	}
-	else {
-		dotList[i - 1].classList.remove("dot_selected")
-		dotList[i].classList.add("dot_selected")
-		changeSlide(slides[i].image, slides[i].tagLine)
-		console.log(i)
-	}
+	console.log("le bouton droite est cliqué")
+	removeCurrentSelectedDot()
+	moveToNextSlide()
+	addCurrentSelectedDot()
+	updateSlideContent()
 });
+
+// arrows.forEach((arrow) => {
+// 	arrow.addEventListener("click", () => {
+// 		console.log("le bouton gauche est cliqué")
+// 		removeCurrentSelectedDot()
+// 		if (arrow.target)
+// 			moveToPreviousSlide()
+// 		addCurrentSelectedDot()
+// 		updateSlideContent()
+// 	})
+// });
