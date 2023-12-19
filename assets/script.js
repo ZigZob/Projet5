2354680563bc0af25f46a247ad206dad027ca9bb
Zig <amalric.alex@gmail.com>
2023-12-19 17:11:51 +0100

    changed index name and removed consolelogs

--- assets/script.js
+++ assets/script.js
@@ -33,10 +33,10 @@ slides.forEach(() => {
 const dotList = Array.from(banner.querySelector(".dots").children)
 
 //initialisation de l'index
-let indexPosition = 0
+let carouselPosition = 0
 
 //initialization de dot_selected
-dotList[indexPosition].classList.add("dot_selected")
+dotList[carouselPosition].classList.add("dot_selected")
 
 //initialization des flèches v2
 // let arrows = Array.from(banner.querySelectorAll(".arrow"))
@@ -58,28 +58,28 @@ function changeBannerTagLine(slideObjectTagLine) {
 
 //mise a jour du contenu de la nouvelle slide
 function updateSlideContent() {
-	changeBannerImg(slides[modulo(indexPosition)].image)
-	changeBannerTagLine(slides[modulo(indexPosition)].tagLine)
+	changeBannerImg(slides[modulo(carouselPosition)].image)
+	changeBannerTagLine(slides[modulo(carouselPosition)].tagLine)
 }
 
 //retrait de la class .dot_selected avant le changement de slide
 function removeCurrentSelectedDot() {
-	dotList[modulo(indexPosition)].classList.remove("dot_selected")
+	dotList[modulo(carouselPosition)].classList.remove("dot_selected")
 }
 
 //ajout de la class .dot_selected a la slide actuelle
 function addCurrentSelectedDot() {
-	dotList[modulo(indexPosition)].classList.add("dot_selected")
+	dotList[modulo(carouselPosition)].classList.add("dot_selected")
 }
 
 //décrémentation de l'index pour passer a la slide précédente
 function moveToPreviousSlide() {
-	indexPosition--
+	carouselPosition--
 }
 
 //incrémentation de l'index pour passer a la slide suivante
 function moveToNextSlide() {
-	indexPosition++
+	carouselPosition++
 }
 
 //modification de l'eventListener en fonction de la fleche cliquée
@@ -87,11 +87,9 @@ function leftOrRight(arrow) {
 	switch (arrow.classList[1]) {
 		case "arrow_left":
 			moveToPreviousSlide()
-			console.log("le bouton gauche est cliqué")
 			break
 		case "arrow_right":
 			moveToNextSlide()
-			console.log("le bouton droit est cliqué")
 			break
 	}
 }
