fd08fd434633f7e744548f9f9cabba930d791249
Zig <amalric.alex@gmail.com>
2023-12-28 17:44:16 +0100

    version finale ... finale ?

--- assets/script.js
+++ assets/script.js
@@ -17,90 +17,84 @@ const slides = [
 	}
 ]
 
-//initialisation du parent
-const banner = document.getElementById("banner")
-
-//création des dot
-slides.forEach(() => {
-	let dot = document.createElement("div")
-	dot.classList.add("dot")
-	banner.querySelector(".dots").append(dot)
-});
-//création de l'array de dots length===slides.length
-const dotList = Array.from(banner.querySelector(".dots").children)
-
-//initialisation de l'index
-let carouselPosition = 0
-
-//initialization de dot_selected
-dotList[carouselPosition].classList.add("dot_selected")
+//initialisation de l'index & de son modulo
+var carouselIndex = 0
+var carouselPosition = modulo(carouselIndex)
 
 //simulation du modulo via l'opérateur remainder pour le défilement infini
 function modulo(value) {
-	return ((value % slides.length) + slides.length) % slides.length
-	//return (Math.abs(value % slides.length)) //alternative
+	return (Math.abs(value % slides.length))
 }
+
+//update de carouselPosition post décrémentation/incrémentation
+function carouselPositionUpdate(carouselIndex) {
+	carouselPosition = modulo(carouselIndex)
+}
+
+//création des dot & initialisation de la dot_selected
+slides.forEach(() => {
+	let dot = document.createElement("div")
+	dot.classList.add("dot")
+	document.querySelector(".dots").append(dot)
+	document.querySelectorAll(".dot")[carouselIndex].classList.add("dot_selected")
+});
 
 //assignation de la source de la bannerImg
 function changeBannerImg(slideObjectImg) {
-	banner.querySelector(".banner-img").src = "./assets/images/slideshow/" + slideObjectImg;
+	document.querySelector(".banner-img").src = "./assets/images/slideshow/" + slideObjectImg;
 }
 
 //assignation du contenu texte du titre
 function changeBannerTagLine(slideObjectTagLine) {
-	banner.getElementsByTagName("p")[0].innerHTML = slideObjectTagLine
+	document.querySelector("#banner p").innerHTML = slideObjectTagLine
 }
 
 //mise a jour du contenu de la nouvelle slide
-function updateSlideContent() {
-	changeBannerImg(slides[modulo(carouselPosition)].image)
-	changeBannerTagLine(slides[modulo(carouselPosition)].tagLine)
+function updateSlideContent(carouselPosition) {
+	changeBannerImg(slides[carouselPosition].image)
+	changeBannerTagLine(slides[carouselPosition].tagLine)
 }
 
 //retrait de la class .dot_selected avant le changement de slide
-function removeCurrentSelectedDot() {
-	dotList[modulo(carouselPosition)].classList.remove("dot_selected")
+function removeCurrentSelectedDot(carouselPosition) {
+	document.querySelectorAll(".dot")[carouselPosition].classList.remove("dot_selected")
 }
 
-//ajout de la class .dot_selected a la slide actuelle
 function addCurrentSelectedDot() {
-	dotList[modulo(carouselPosition)].classList.add("dot_selected")
+	document.querySelectorAll(".dot")[carouselPosition].classList.add("dot_selected")
 }
 
 //décrémentation de l'index pour passer a la slide précédente
 function moveToPreviousSlide() {
-	carouselPosition--
+	carouselIndex--
 }
 
 //incrémentation de l'index pour passer a la slide suivante
 function moveToNextSlide() {
-	carouselPosition++
+	carouselIndex++
 }
 
 //modification de l'eventListener en fonction de la fleche cliquée
-function leftOrRight(arrow) {
-	switch (arrow.classList[1]) {
-		case "arrow_left":
+function leftOrRight(eventTarget) {
+	switch (eventTarget.getAttribute("data-carouselArrow")) {
+		case "left":
 			moveToPreviousSlide()
 			break
-		case "arrow_right":
+		case "right":
 			moveToNextSlide()
+			break
+		default:
 			break
 	}
 }
 
-//eventListener des flèches
-function arrowEventListeners() {
-	let arrows = Array.from(banner.querySelectorAll(".arrow"))
-	arrows.forEach((arrow) => {
-		arrow.addEventListener("click", () => {
-			removeCurrentSelectedDot()
-			leftOrRight(arrow)
-			addCurrentSelectedDot()
-			updateSlideContent()
-		})
-	});
-}
-
-//initialisation des eventListeners sans garder l'array de fleches en memoire
-arrowEventListeners()
+//création des eventListener pour chaque fleche
+document.querySelectorAll(".arrow").forEach(arrow => {
+	arrow.addEventListener("click", event => {
+		removeCurrentSelectedDot(carouselPosition)
+		leftOrRight(event.target)
+		carouselPositionUpdate(carouselIndex)
+		addCurrentSelectedDot(carouselPosition)
+		updateSlideContent(carouselPosition)
+	})
+});
